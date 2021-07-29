9b66d5538484c85738b1c534c963aa2c858c39a1
SangYong-Jeong <mmaf@naver.com>
2021-07-29 11:25:02 +0900

    2교시 끝

--- js/04.if.js
+++ js/04.if.js
@@ -1,10 +1,30 @@
 /* 
 비교연산자
-==, <=, >=, <, >, === (타입까지 같아야 함)
+==, <=, >=, <, >, === (타입까지 같아야 함), != , !==
 
 - 내장객체
 window, console, String, Number, Array, Object, Date, Math
 window.document 
+
+- 변수의 타입
+
+
+console.clear();
+var a = '10';
+if (typeof a !== 'number') {
+	console.log('변수 a는 숫자가 아닙니다.');
+}
+else if (a < 10) {
+	console.log('변수 a는 10 미만의 수 입니다.');
+}
+else if (a > 10) {
+	console.log('변수 a는 10 초과의 수 입니다.');
+}
+else {
+	console.log('변수 a는 10입니다.');
+}
+
+
 */
 
 console.log(window);
@@ -17,13 +37,30 @@ console.log(Object);
 console.log(Date);
 console.log(Math);
 
-function changeColor (value) {
+/* function changeColor (value) {
 	// alert(value === '');
 	if(value === '') {
 		console.log('value는 빈문자열 입니다.');
-		console.log(document.getElementById('box'));
+		document.getElementById('box').style.backgroundColor = 'transparent';
 	}
 	else {
 		console.log('value는 ' + value + '입니다.');
+		document.getElementById('box').style.backgroundColor = value;
 	}
+ */
+
+function changeColor (value) {
+	
+	if(value === '') {
+		document.getElementById('box').style.backgroundColor = 'transparent';
+	}
+	else {
+		document.getElementById('box').style.backgroundColor = value;
+	}
+}
+
+console.clear();
+
+function showImg(src) {
+	document.getElementById('mainImg').src = src;
 }
